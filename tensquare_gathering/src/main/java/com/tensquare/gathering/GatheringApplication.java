458555d13fb15e17cdd2829cfbd310e301bdf487
noShampoo <2551480456@qq.com>
2020-05-11 10:31:03 +0800

    添加活动子模块的业务逻辑代码，并在活动模块中使用SpringCacha

---
+++ tensquare_gathering/src/main/java/com/tensquare/gathering/GatheringApplication.java
@@ -0,0 +1,26 @@
+package com.tensquare.gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import util.IdWorker;
+
+/**
+ * 注解@EnableCaching代表启用SpringCache，然后在对应的Service上使用，需要注意
+ * SpringCache没有过期时间，一般针对findById使用
+ */
+@SpringBootApplication
+@EnableCaching
+public class GatheringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatheringApplication.class, args);
+    }
+
+    @Bean
+    public IdWorker idWorkker() {
+        return new IdWorker(1, 1);
+    }
+
+}
