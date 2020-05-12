561fb6e4b02c06212fc0effbbf3b94b19fcf604f
noShampoo <2551480456@qq.com>
2020-05-12 21:21:18 +0800

    完成查找模块按关键词查找的业务逻辑代码

--- tensquare_search/src/main/java/com/tensquare/search/controller/ArticleController.java
+++ tensquare_search/src/main/java/com/tensquare/search/controller/ArticleController.java
@@ -2,9 +2,11 @@ package com.tensquare.search.controller;
 
 import com.tensquare.search.service.ArticleService;
 import com.tensquare.search.pojo.Article;
+import entity.PageResult;
 import entity.Result;
 import entity.StatusCode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -19,6 +21,15 @@ public class ArticleController {
     public Result save(@RequestBody Article article) {
         articleService.save(article);
         return new Result(true, StatusCode.OK, "添加成功");
+    }
+
+    @GetMapping("/{key}/{page}/{size}")
+    public Result findByKey(@PathVariable("key") String key,
+                            @PathVariable("page") int page,
+                            @PathVariable("size") int size) {
+        Page<Article> articlePage = articleService.findByKey(key, page, size);
+        return new Result(true, StatusCode.OK, "查询成功",
+                new PageResult<Article>(articlePage.getTotalElements(), articlePage.getContent()));
     }
 
 }
--- tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
+++ tensquare_search/src/main/java/com/tensquare/search/service/ArticleService.java
@@ -3,6 +3,8 @@ package com.tensquare.search.service;
 import com.tensquare.search.dao.ArticleDao;
 import com.tensquare.search.pojo.Article;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import util.IdWorker;
 
@@ -15,9 +17,23 @@ public class ArticleService {
     @Autowired
     private IdWorker idWorker;
 
+    /**
+     * 保存新数据
+     * @param article
+     */
     public void save(Article article) {
         article.setId(idWorker.nextId() + "");
         articleDao.save(article);
     }
 
+    /**
+     * 通过关键字或搜索字在标题和内容中查找
+     * @param key
+     * @param page
+     * @param size
+     * @return
+     */
+    public Page<Article> findByKey(String key, int page, int size) {
+        return articleDao.findByTitleOrContentLike(key, key, PageRequest.of(page - 1, size));
+    }
 }
